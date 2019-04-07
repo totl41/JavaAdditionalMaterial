9dc2eeaf6533917c89287bda9fdcbadcf6ba2fea
totl41 <naumovbox@gmail.com>
2019-04-07 15:14:47 +0300

    Эксперементы со строкой

---
+++ src/HelloWold.java
@@ -0,0 +1,14 @@
+public class HelloWold {
+
+    public static void main(String[] args){
+
+        int x =1, y = 2, z = 3;
+
+        java.lang.String s1 = "Hello";
+        java.lang.String s2 = "wold";
+
+        System.out.println(s1 + s2);
+
+    }
+
+}
